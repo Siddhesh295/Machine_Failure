b462bd263e0ecb12ca4de7678fa1f976867ad305
Siddhesh295 <155010094+Siddhesh295@users.noreply.github.com>
2024-06-24 17:19:22 +0530

    Update streamlit.py

--- streamlit.py/streamlit.py
+++ streamlit.py/streamlit.py
@@ -31,8 +31,7 @@ st.markdown(
 )
 
 # Load the model
-with open('Model/model.pkl', 'rb') as model_file:
-     Model = pickle.load(model_file)
+model = pickle.load(open('Model/model.pkl', 'rb'))
 
 # Streamlit interface to input data
 col1, col2 = st.columns(2)
