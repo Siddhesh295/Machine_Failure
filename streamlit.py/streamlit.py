4188a3d03c6ee121a5ad538128bc33a352d085cc
Siddhesh295 <155010094+Siddhesh295@users.noreply.github.com>
2024-06-27 20:42:32 +0530

    Update streamlit.py

--- streamlit.py/streamlit.py
+++ streamlit.py/streamlit.py
@@ -24,7 +24,7 @@ st.markdown(
 st.write(f"Current working directory: {os.getcwd()}")
 
 # Check if the model file exists
-model_path = 'Model/model (3).pkl'
+model_path = 'Model/model (4).pkl'
 if not os.path.exists(model_path):
     st.error(f"Model file not found at path: {model_path}")
 
